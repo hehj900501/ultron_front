8d54f57c97050c826e349282e65fa464ef6b3da0
hehj900501 <hehj900501@gmail.com>
2023-07-02 16:52:05 -0600

    Expediente electronico

--- src/containers/dermatologos/ficha_clinica/expediente_electronico/index.js
+++ src/containers/dermatologos/ficha_clinica/expediente_electronico/index.js
@@ -4,6 +4,7 @@ import { ExpedienteElectronicoContainer } from "./expediente_electronico";
 import { Backdrop, CircularProgress, Snackbar } from "@material-ui/core";
 import MuiAlert from '@material-ui/lab/Alert';
 import { useNavigate } from "react-router-dom";
+import { createExpedienteElectronico, updateExpedienteElectronico } from "../../../../services/u-sgcm-ficha-clinica/expediente_electronico";
 
 function Alert(props) {
 	return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -31,20 +32,49 @@ const ExpedienteElectronico = (props) => {
 	const {
 		consultorio,
 		colorBase,
+		historiaClinica,
+		commitHistoriaClinica,
+		findConsultorio,
 	} = props
 
 	const [openAlert, setOpenAlert] = useState(false)
 	const [message, setMessage] = useState('')
 	const [severity, setSeverity] = useState('success')
 	const [isLoading, setIsLoading] = useState(true)
+	const [expedienteElectronico, setExpedienteElectronico] = useState({})
 
 	const handleCloseAlert = () => {
 		setOpenAlert(false)
 	};
 
+	const handleChange = (event) => {
+		setExpedienteElectronico({
+			...expedienteElectronico,
+			[event.target.name]: event.target.value.toUpperCase()
+		})
+	}
+
+	const handleClickGuardar = async() => {
+		setIsLoading(true)
+		let responseExpedienteElectronico = {}
+		if (!expedienteElectronico._id) {
+			responseExpedienteElectronico = await createExpedienteElectronico(expedienteElectronico)
+			if (`${responseExpedienteElectronico.status}` === process.env.REACT_APP_RESPONSE_CODE_CREATED) {
+				historiaClinica.expediente_electronico = responseExpedienteElectronico.data._id
+				await commitHistoriaClinica()
+			}
+		} else {
+			responseExpedienteElectronico = await updateExpedienteElectronico(expedienteElectronico._id, expedienteElectronico)
+			if (`${responseExpedienteElectronico.status}` === process.env.REACT_APP_RESPONSE_CODE_OK) {
+				findConsultorio()
+			}
+		}
+		setIsLoading(false)
+	}
+
 	const loadAll = async () => {
 		setIsLoading(true)
-
+		setExpedienteElectronico(historiaClinica.expediente_electronico ? historiaClinica.expediente_electronico : {}) 
 		setIsLoading(false)
 	}
 
@@ -58,7 +88,10 @@ const ExpedienteElectronico = (props) => {
 				!isLoading ?
 					<ExpedienteElectronicoContainer
 						consultorio={consultorio}
-						colorBase={colorBase} /> :
+						colorBase={colorBase}
+						expedienteElectronico={expedienteElectronico}
+						onChange={handleChange}
+						onClickGuardar={handleClickGuardar} /> :
 					<Backdrop className={classes.backdrop} open={isLoading} >
 						<CircularProgress color="inherit" />
 					</Backdrop>
